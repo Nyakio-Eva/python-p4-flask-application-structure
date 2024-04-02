f6d6e3daf2fca7bff6a40cd32f9f05abad98b4a9
Nyakio-Eva <evalkariuki@gmail.com>
2024-04-02 21:24:06 +0300

    completed assighnment

--- server/app.py
+++ server/app.py
@@ -3,3 +3,16 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my app!</h1>'
+
+@app.route('/profile/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</hi>'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
+
+    
